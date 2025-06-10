5b39ce5da5c43dd70c383f00d9a7d1d0ddeb2b66
Xryzo11 <lukowskis11@gmail.com>
2025-06-10 20:59:27 +0200

    comment update

--- cw/dynarray.c
+++ cw/dynarray.c
@@ -3,10 +3,15 @@
 #include <time.h>
 
 int* createDynamicArray(int size) {
+    // (rozmiar w elementach) * (rozmiar typu tablicy)
+    // w przypadku np float, musialoby byc:
+    // malloc(size * sizeof(float));
+    // bo kazdy typ ma inny tozmiar w pamieci
     int *array = malloc(size * sizeof(int));
     return array;
 }
 
+// void bo bezposrednio modyfikuje tablice
 void fillArray(int* array, int size) {
     for (int i = 0; i < size; i++) {
         array[i] = 0;
